65a25bbded895088eeb7753764dfb384c9b03a20
Marko Maglic <mm50339@fer.hr>
2022-01-25 18:11:41 +0100

    fixes 3

---
+++ labos5 - Copy/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const httpPort = process.env.PORT  || 80;
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+app.listen(httpPort, function () {
+  console.log(`Listening on port ${httpPort}!`)
+})
